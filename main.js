236cb9c05fd52f9766b6eec6be85e7d1e4812026
Dirk Dobson <dirkvancouver@live.com>
2018-08-21 16:09:23 -0600

    inital commit

---
+++ main.js
@@ -0,0 +1,84 @@
+$(document).ready( function() {
+
+var options = ['rock', 'paper', 'scissors']
+var score = {win: 0, lose: 0, tie: 0 }
+var $rock = $('rock')
+var $paper = $('paper')
+var $scissors = $('scissors')
+var user = ''
+var computer = ''
+var button = $('button')
+
+
+
+
+    $rock.on('click', function() {
+      user = 'rock'
+      computer = computerSelection()
+      result = compare()
+      score[result]++
+      render()
+    })
+    $paper.on('click', function() {
+      user = 'paper'
+      computer = computerSelection()
+      result = compare()
+      score[result]++
+      render()
+    })
+    $scissors.on('click', function() {
+      user = 'scissors'
+      computer = computerSelection()
+      result = compare()
+      score[result]++
+      render()
+    })
+
+   
+
+function computerSelection() {
+  return options[Math.floor(Math.random() * options.length)]
+}
+
+function compare() {
+  if (user === computer)
+    return 'tie' 
+
+  switch(user) {
+    case 'rock':
+      return computer === 'paper' ? 'lose' : 'win'
+    case 'paper':
+      return computer === 'scissors' ? 'lose' : 'win'
+    case 'scissors':
+      return computer === 'rock' ? 'lose' : 'win'
+    default:
+      return
+  }
+}
+ 
+function render() {
+  win.innerHTML = 'Wins: ' + score.win
+  lose.innerHTML = 'Losses: ' + score.lose
+  tie.innerHTML = 'Ties: ' + score.tie
+  it.innerHTML = 'It Picked: ' + computer
+
+}
+
+for (var i = 0; i < button.length; i++) {
+  // [#rock.choice, #paper.choice, #scissors.choice]
+  button[i].addEventListener('click', function() {
+    user = computerSelection()
+computer = computerSelection()
+result = compare()
+score[result]++
+render()
+})}
+
+
+
+
+
+
+
+
+})
